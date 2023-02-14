68d34b929f21b69cf69eb98c40cb5201bb5d2822
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2023-02-14 23:38:17 +0100

    fix: Added error handling when path is too big

--- src/commands/dele.c
+++ src/commands/dele.c
@@ -17,6 +17,8 @@ client_t *client, UNUSED server_t *serv)
         return free(cmd), dputs(RESPONSE_NOT_LOGGED_IN, client->fd);
     if (strlen(cmd) <= 2 || command[4] != ' ')
         return free(cmd), dputs(RESPONSE_UNKNOW_CMD, client->fd);
+    if (strlen(client->cwd) + strlen(cmd) + 2 > PATH_MAX)
+        return free(cmd), dputs(RESPONSE_FILE_LOCAL_ERROR, client->fd);
     cmd[strlen(cmd) - 2] = '\0';
     filePath = malloc(strlen(client->cwd) + strlen(cmd) + 2);
     sprintf(filePath, "%s/%s", client->cwd, cmd);
